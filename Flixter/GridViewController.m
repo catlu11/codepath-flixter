8e10b5874827840778c295d10a6d1cad0b590558
Catherine Lu <catherinelu@fb.com>
2022-06-17 10:51:43 -0700

    Added search bar functionality to Movies collection

--- Flixter/GridViewController.m
+++ Flixter/GridViewController.m
@@ -10,9 +10,11 @@
 #import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 
-@interface GridViewController () <UICollectionViewDataSource>
-@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
-@property (strong, nonatomic) NSArray *posterData;
+@interface GridViewController () <UICollectionViewDataSource, UISearchBarDelegate>
+    @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+    @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+    @property (strong, nonatomic) NSArray *posterData;
+    @property (strong, nonatomic) NSArray *filteredPosterData;
 @end
 
 @implementation GridViewController
@@ -21,6 +23,7 @@
     [super viewDidLoad];
 
     self.collectionView.dataSource = self;
+    self.searchBar.delegate = self;
     
     // API call
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=99c35d01f55c02ab3de5e8b1c7d5b6c8"];
@@ -35,6 +38,7 @@
 
            NSArray *movieArray = dataDictionary[@"results"];
            self.posterData = movieArray;
+           self.filteredPosterData = self.posterData;
            
            [self.collectionView reloadData];
        }
@@ -43,7 +47,7 @@
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    NSDictionary *dataToPass = self.posterData[self.collectionView.indexPathsForSelectedItems[0].item];
+    NSDictionary *dataToPass = self.filteredPosterData[self.collectionView.indexPathsForSelectedItems[0].item];
     DetailsViewController *detailVC = [segue destinationViewController];
     detailVC.detailsDict = dataToPass;
 }
@@ -52,7 +56,7 @@
     MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
     
     NSString *baseURL = @"https://image.tmdb.org/t/p/w500";
-    NSString *posterURL = self.posterData[indexPath.item][@"poster_path"];
+    NSString *posterURL = self.filteredPosterData[indexPath.item][@"poster_path"];
     NSString *stringURL = [baseURL stringByAppendingString:posterURL];
     NSURL *imageURL = [NSURL URLWithString:stringURL];
     
@@ -62,7 +66,33 @@
 }
 
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.posterData.count;
+    return self.filteredPosterData.count;
+}
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = YES;
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = NO;
+    self.searchBar.text = @"";
+    self.filteredPosterData = self.posterData;
+    [self.collectionView reloadData];
+    [self.searchBar resignFirstResponder];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *evaluatedObject, NSDictionary *bindings) {
+            NSString *title = evaluatedObject[@"original_title"];
+            return [title rangeOfString:searchText options:NSCaseInsensitiveSearch].location != NSNotFound;
+        }];
+        self.filteredPosterData = [self.posterData filteredArrayUsingPredicate:predicate];
+    }
+    else {
+        self.filteredPosterData = self.posterData;
+    }
+    [self.collectionView reloadData];
 }
 
 @end
